c53a41a2aa8481d2301327be3490cad9645814ff
Naimul9 <naimulakib100@gmail.com>
2024-03-26 04:40:52 +0600

    react icon installed

--- src/Components/SingleBook/SingleBook.jsx
+++ src/Components/SingleBook/SingleBook.jsx
@@ -16,17 +16,17 @@ const SingleBook = ({item}) => {
 				<div className="flex flex-col flex-1 mt-7">
 					
 				<div className="flex gap-6">
-                <p className="">{tags[0]}</p>
-					<p className="">{tags[1]}</p>
+                <p className="w-[130px] h-[34px] bg-[#23BE0A0D] text-[#23BE0A] rounded-full text-center py-1 font-medium">{tags[0]}</p>
+					<p className="w-[125px] h-[34px] bg-[#23BE0A0D] text-[#23BE0A] rounded-full text-center py-1 font-medium">{tags[1]}</p>
                 </div>
 
-					<h3 className="flex-1 py-2 text-lg font-semibold ">{bookName}</h3>
-                    <p>By : {author}</p>
+					<h3 className="flex-1 py-2 text-2xl font-bold ">{bookName}</h3>
+                    <p className="font-medium">By : {author}</p>
 
 
 					<div className="flex flex-wrap justify-between  text-gray-400 mt-10">
-						<span>June 1, 2020</span>
-						<span>2.1K views</span>
+						<span>{category} </span>
+						<p><span>2.1K views</span></p>
 					</div>
 				</div>
 			</article>
